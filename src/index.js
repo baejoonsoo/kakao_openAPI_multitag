1956fbc5fccfc9ba104d2e50c3d9af1d9bd9f160
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-29 21:49:32 +0900

    font 추가

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./page/App";
 import "./style/index.css";
+import "./style/font.css";
 
 ReactDOM.render(
   <React.StrictMode>
